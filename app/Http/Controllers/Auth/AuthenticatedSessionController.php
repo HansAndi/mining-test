1d985efa847a7b8852ab132cc6d352f1e8fd79b7
HansAndi <hansandiwijaya@gmail.com>
2024-08-28 11:02:11 +0700

    fix: alert toast

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -28,6 +29,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        Alert::toast('Welcome back!', 'success');
+
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
@@ -42,6 +45,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        Alert::toast('You have been logged out!', 'success');
+
+        return redirect('/login');
     }
 }
